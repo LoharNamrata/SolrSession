9d563bbe9381ddf29809f47b465e4c22e5dbf74d
LoharNamrata <namratalohar@pragiti.com>
2019-07-28 13:52:53 +0530

    T1_6 -> Added default filter query(fq) attribute for the PLP products (fq)

---
+++ hybris/bin/custom/SolrSession/SolrSessionfacades/src/com/solrsession/facades/populators/SolrSessionSearchFiltersPopulator.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+package com.solrsession.facades.populators;
+
+import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SearchQueryPageableData;
+import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SolrSearchQueryData;
+import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SolrSearchRequest;
+import de.hybris.platform.commerceservices.search.solrfacetsearch.populators.SearchFiltersPopulator;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.config.IndexedType;
+import de.hybris.platform.solrfacetsearch.search.SearchQuery;
+
+
+/**
+ * @author admin
+ *
+ */
+public class SolrSessionSearchFiltersPopulator<FACET_SEARCH_CONFIG_TYPE, INDEXED_TYPE_SORT_TYPE>
+		extends SearchFiltersPopulator<FACET_SEARCH_CONFIG_TYPE, INDEXED_TYPE_SORT_TYPE>
+{
+
+	@Override
+	public void populate(final SearchQueryPageableData<SolrSearchQueryData> source,
+			final SolrSearchRequest<FACET_SEARCH_CONFIG_TYPE, IndexedType, IndexedProperty, SearchQuery, INDEXED_TYPE_SORT_TYPE> target)
+	{
+		super.populate(source, target);
+		if (target.getSearchQueryData().getCategoryCode() != null)
+		{
+			// allCategories field indexes all the separate category hierarchies
+			target.getSearchQuery().addFilterQuery("prior", "5");
+		}
+	}
+
+}
