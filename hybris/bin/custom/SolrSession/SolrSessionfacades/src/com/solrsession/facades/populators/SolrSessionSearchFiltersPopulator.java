9139b012b989f8f7592cc88b0ddc0dff18302d66
LoharNamrata <namratalohar@pragiti.com>
2019-08-07 15:51:39 +0530

    #T1_6 -> Update

--- hybris/bin/custom/SolrSession/SolrSessionfacades/src/com/solrsession/facades/populators/SolrSessionSearchFiltersPopulator.java
+++ hybris/bin/custom/SolrSession/SolrSessionfacades/src/com/solrsession/facades/populators/SolrSessionSearchFiltersPopulator.java
@@ -6,7 +6,7 @@ package com.solrsession.facades.populators;
 import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SearchQueryPageableData;
 import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SolrSearchQueryData;
 import de.hybris.platform.commerceservices.search.solrfacetsearch.data.SolrSearchRequest;
-import de.hybris.platform.commerceservices.search.solrfacetsearch.populators.SearchFiltersPopulator;
+import de.hybris.platform.converters.Populator;
 import de.hybris.platform.solrfacetsearch.config.IndexedType;
 import de.hybris.platform.solrfacetsearch.search.SearchQuery;
 
@@ -17,19 +17,18 @@ import groovy.transform.IndexedProperty;
  * @author admin
  *
  */
-public class SolrSessionSearchFiltersPopulator<FACET_SEARCH_CONFIG_TYPE, INDEXED_TYPE_SORT_TYPE>
-		extends SearchFiltersPopulator<FACET_SEARCH_CONFIG_TYPE, INDEXED_TYPE_SORT_TYPE>
+public class SolrSessionSearchFiltersPopulator<FACET_SEARCH_CONFIG_TYPE, INDEXED_TYPE_SORT_TYPE> implements
+		Populator<SearchQueryPageableData<SolrSearchQueryData>, SolrSearchRequest<FACET_SEARCH_CONFIG_TYPE, IndexedType, IndexedProperty, SearchQuery, INDEXED_TYPE_SORT_TYPE>>
 {
-
 	@Override
 	public void populate(final SearchQueryPageableData<SolrSearchQueryData> source,
 			final SolrSearchRequest<FACET_SEARCH_CONFIG_TYPE, IndexedType, IndexedProperty, SearchQuery, INDEXED_TYPE_SORT_TYPE> target)
 	{
-		super.populate(source, target);
+		//super.populate(source, target);
 		if (target.getSearchQueryData().getCategoryCode() != null)
 		{
 			// allCategories field indexes all the separate category hierarchies
-			//target.getSearchQuery().addFilterQuery("prior", "5");
+			target.getSearchQuery().addFilterQuery("prior", "5");
 		}
 	}
 
